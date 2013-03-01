5c44af024f1177ffd191f1041e617e4771a43b7b
maisaengineering <contact@maisasolutions.com>
2013-03-01 11:47:39 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyKidslink::Application.config.secret_token = '5f060dd73c5c287566c67c3985a09a11fbb086f4171d333bd4e59c61d79308d410119ab2a52e59577644fbb766f87c1a0c9aa99cf6c990e40e70649a296a2026'
